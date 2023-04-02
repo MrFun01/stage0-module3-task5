f11941402265a1b0d2fa1d91d3656529213a4f55
MrFun01 <hprobook48@gmail.com>
2023-04-02 16:02:03 +0500

    Finished task 5 on the module 3

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        double n = toBeDivided;
+        System.out.println(n/5);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds=60;
+        System.out.println(minutes*seconds);
     }
 }
